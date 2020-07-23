4aeef0be7d2a9fe40e40d97c4ee265e04abbae20
Kern Elder <kelel1@yahoo.com>
2020-07-23 16:18:26 -0400

    refractor code

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,15 @@
 const Hero = () => (
   <div className="container">
-    <p>Hello</p>
-    <p className="kern">My name is Kern Elder.</p>
+    <p>Hello My name is</p>
+    <p className="kern">Kern Elder.</p>
     <p>Welcome to my portfolio!</p>
   <style jsx>{`
     .container {
       width: 100%;
-      height: 50%;
+      margin-top: 10%;
     }  
     .kern {
-      font-size: 50px;
+      font-size: 30px;
     }
   `}</style>
   </div>
